dec6c0d1c66cdf8b2d93168c4fca719c717f0840
AndreevskiDamjan <andreevski21@gmail.com>
2023-03-10 17:44:06 +0100

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Please insert two numbers.");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int result;
+Console.WriteLine("Please insert ans operator: +, -, / or * ");
+string operationSymbol = Console.ReadLine();
+if (operationSymbol == "+")
+{
+    result = firstNumber + secondNumber;
+    Console.WriteLine(result);
+}
+if (operationSymbol == "-")
+{
+    result = firstNumber - secondNumber;
+    Console.WriteLine(result);
+}
+if (operationSymbol == "*")
+{
+    result = firstNumber * secondNumber;
+    Console.WriteLine(result);
+}
+if (operationSymbol == "/")
+{
+    result = firstNumber / secondNumber;
+    Console.WriteLine(result);
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Please insert 4 numbers to find out their average.");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int fourthNumber = Convert.ToInt32(Console.ReadLine());
+int average = (firstNumber + secondNumber + thirdNumber + fourthNumber) / 4;
+Console.WriteLine("The average of " + firstNumber + ", " + secondNumber + ", " + thirdNumber + " and " + fourthNumber + " is: " + average);
